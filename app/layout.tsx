680474cbf76b152457ec82d5a64799ba5ce93c09
beardednotary <thebeardednotary@gmail.com>
2026-08-04 23:49:10 -0700

    Added Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
+
+const GA_MEASUREMENT_ID = "G-79Q4GK6S6P";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://thegardenmd.com"),
@@ -27,6 +30,15 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
